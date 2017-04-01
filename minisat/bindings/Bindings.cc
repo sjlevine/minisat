5159e53471d045501bb451bc1e57954fb959e576
Steve Levine <sjlevine@mit.edu>
2017-04-01 16:32:23 -0400

    Update to 1-based counting

--- minisat/bindings/Bindings.cc
+++ minisat/bindings/Bindings.cc
@@ -2,6 +2,7 @@
 These C bindings were written by Steve Levine -- please see license in other files.
 **************************************************************************************************/
 
+#include "minisat/utils/System.h"
 #include "minisat/core/SolverTypes.h"
 #include "minisat/simp/SimpSolver.h"
 
@@ -19,6 +20,12 @@ extern "C" void minisat_add_current_clause();
 
 extern "C" bool minisat_solve();
 
+extern "C" int minisat_nvars();
+
+extern "C" int minisat_nclauses();
+
+extern "C" int minisat_nlearnts();
+
 
 //=================================================================================================
 
@@ -32,9 +39,11 @@ static vec<Lit> lits;
 // Main Bindings:
 
 void minisat_initialize_new_solver() {
+  // setX86FPUPrecision();
   if (solver) {
     delete solver;
   }
+  lits.clear();
   solver = new SimpSolver;
   solver->verbosity = 0; // Set to silent
 }
@@ -51,6 +60,8 @@ void minisat_begin_new_clause() {
 }
 
 void minisat_add_literal_to_current_clause(int var, bool positive) {
+  var--; // Variables are integer based in Minisat, but we assume they're
+         // 1 based in input
   while (var >= solver->nVars()) {solver->newVar();}
   lits.push( positive ? mkLit(var) : ~mkLit(var) );
 }
@@ -62,12 +73,25 @@ void minisat_add_current_clause() {
 
 
 bool minisat_solve() {
+  // We could optionally set some resource limits (CPU, memory) here
   solver->eliminate(true);
   if (!solver->okay()) {
     // Unsatisfiable!
     return false;
   }
   vec<Lit> dummy;
-  bool ret = solver->solve(dummy);
-  return ret;
+  lbool ret = solver->solveLimited(dummy);
+  return (ret == l_True);
+}
+
+int minisat_nvars() {
+  return solver->nVars();
+}
+
+int minisat_nclauses() {
+  return solver->nClauses();
+}
+
+int minisat_nlearnts() {
+  return solver->nLearnts();
 }
